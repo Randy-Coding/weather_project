69883908c4b03cd1894192c25150b8bb0fbfdda0
Randy-Coding <164274204+Randy-Coding@users.noreply.github.com>
2024-03-26 21:44:59 -0400

    Testing with catboosting
    Trying out catboosting as well as changing my data preparer to better fit my intended use

--- Testing/Ensemble_Testing.py
+++ Testing/Ensemble_Testing.py
@@ -7,6 +7,7 @@ from sklearn.ensemble import (
 )
 import joblib
 from joblib import dump
+from catboost import CatBoostRegressor
 from sklearn.model_selection import cross_validate
 from sklearn.metrics import mean_absolute_error, mean_squared_error
 import numpy as np
@@ -29,42 +30,68 @@ def test_data(model, model_name: str, training_data, testing_data, cv=5):
     y_test = testing_data["target_temp"]
 
     # Define scoring metrics
-    scoring = {"MAE": "neg_mean_absolute_error", "MSE": "neg_mean_squared_error"}
+    # scoring = {"MAE": "neg_mean_absolute_error", "MSE": "neg_mean_squared_error"}
 
     # Perform cross-validation
-    cv_results = cross_validate(
-        model, X_train, y_train, cv=cv, scoring=scoring, return_train_score=False
-    )
+    # cv_results = cross_validate(
+    #     model, X_train, y_train, cv=cv, scoring=scoring, return_train_score=False
+    # )
 
     # Compute RMSE scores from MSE scores
-    cv_rmse_scores = np.sqrt(-cv_results["test_MSE"])
+    # cv_rmse_scores = np.sqrt(-cv_results["test_MSE"])
 
     # Convert MAE scores to positive values
-    cv_mae_scores = -cv_results["test_MAE"]
+    # cv_mae_scores = -cv_results["test_MAE"]
 
-    print(f"\nCross-Validation Results for: {model_name}")
-    print(f"MAE scores: {cv_mae_scores}")
-    print(f"Mean MAE: {cv_mae_scores.mean()}")
-    print(f"RMSE scores: {cv_rmse_scores}")
-    print(f"Mean RMSE: {cv_rmse_scores.mean()}")
-    print(f"Standard deviation (RMSE): {cv_rmse_scores.std()}")
+    # print(f"\nCross-Validation Results for: {model_name}")
+    # print(f"MAE scores: {cv_mae_scores}")
+    # print(f"Mean MAE: {cv_mae_scores.mean()}")
+    # print(f"RMSE scores: {cv_rmse_scores}")
+    # print(f"Mean RMSE: {cv_rmse_scores.mean()}")
+    # print(f"Standard deviation (RMSE): {cv_rmse_scores.std()}")
 
     # Training the model on the full training data and evaluate on the test set
     model.fit(X_train, y_train)
     y_pred = model.predict(X_test)
 
-    print(f"\n\nTest Set Results for: {model_name}")
+    errors = y_test - y_pred
+
+    # Round errors to nearest integer
+    rounded_errors = errors.round(0).abs()
+
+    # Find how many errors are above 20
+    sum_of_errors = 0
+    for error in rounded_errors:
+        if error > 10:
+            sum_of_errors += 1
+
+    print(sum_of_errors)
+
+    # print(f"\n\nTest Set Results for: {model_name}")
     # Calculate MAE
-    mae = mean_absolute_error(y_test, y_pred)
-    print(f"Mean Absolute Error (MAE): {mae}")
+    # mae = mean_absolute_error(y_test, y_pred)
+    # print(f"Mean Absolute Error (MAE): {mae}")
     # Calculate MSE
-    mse = mean_squared_error(y_test, y_pred)
-    print(f"Mean Squared Error (MSE): {mse}")
+    # mse = mean_squared_error(y_test, y_pred)
+    # print(f"Mean Squared Error (MSE): {mse}")
     # Calculate RMSE
-    rmse = np.sqrt(mse)
-    print(f"Root Mean Squared Error (RMSE): {rmse}")
+    # rmse = np.sqrt(mse)
+    # print(f"Root Mean Squared Error (RMSE): {rmse}")
+
+    error_counts = rounded_errors.value_counts().sort_index()
+
+    # Plot frequency of each error
+    plt.plot(error_counts.index, error_counts.values)
+    plt.title("Frequency of Rounded Errors")
+    plt.xlabel("Error")
+    plt.ylabel("Frequency")
+    plt.show()
 
 
-model = RandomForestRegressor(n_estimators=200, random_state=42)
-test_data(model, "Random Forest Regression", training_data, testing_data)
-dump(model, "Random_Forest_Model.pkl")
+# create a catboost model with a depth of 10
+model = CatBoostRegressor(
+    iterations=200, learning_rate=0.5, depth=8, random_state=42, verbose=False
+)
+test_data(model, "CatBoost Regression", training_data, testing_data)
+
+dump(model, "Catboost_Regressor.pkl")
